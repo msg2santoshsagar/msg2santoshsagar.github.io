627b007b2c193e581529c551352c9d09f03f647c
Santosh Sagar <msg2santoshsagar@gmail.com>
2025-04-07 07:35:18 +0530

    Master commit - Added only hello world

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+    return (
+        <main className="min-h-screen w-full flex items-center justify-center bg-white px-4">
+            <h1 className="text-5xl font-semibold text-gray-800 text-center text-blue-600 hover:text-blue-700 transition-colors duration-300">
+                Hi, I'm Santosh Sagar 👋
+            </h1>
+        </main>
+    )
+}
+
+export default App
